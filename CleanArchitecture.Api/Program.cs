95d3d25507fc6f6135451d8a98cf1961e82a4b9b
jraimondo <jraimondo@correo.dnic.gub.uy>
2022-10-17 14:35:34 -0300

    segundo commit con minor fix

--- CleanArchitecture.Api/Program.cs
+++ CleanArchitecture.Api/Program.cs
@@ -1,4 +1,5 @@
 using CleanArchitecture.Application;
+using CleanArchitecture.Infraestructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddApplicationServices();
-
+builder.Services.AddInfraestructureServices(builder.Configuration);
 
 var app = builder.Build();
 
